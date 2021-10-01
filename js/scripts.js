acd67617eb1e8347f99fb36aa4719dfeee41aac3
Morgan <morgan8908@gmail.com>
2021-10-01 16:39:23 -0600

    update jquery functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
 console.log("Oh Hey! Welcome to my porfolio. I'm thrilled you're here!");
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+    if (x.className === 'navtoggle') {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+}
+$(function(){
+  $.scrollUp();
+});
+$(function() {
+
+    // Default
+    jQuery.scrollSpeed(100, 800);
+
+    // Custom Easing
+    jQuery.scrollSpeed(100, 800, 'easeOutCubic');
+
+});
