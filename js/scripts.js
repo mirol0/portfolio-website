c1b8b2b4bf473e2c23c3e2bdccbbfea211e0e63e
Morgan <morgan8908@gmail.com>
2021-10-12 17:28:59 -0600

    fix nav menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
-console.log("Oh Hey! Welcome to my porfolio. I'm thrilled you're here!");
 function menuToggle(){
   var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
+    if (x.className === 'navtoggle' && window.innerWidth <640) {
       x.className += ' responsive';
     } else {
       x.className = 'navtoggle';
     }
   }
-$(function(){
-  $.scrollUp();
-});
-$(function() {
 
-    // Default
-    jQuery.scrollSpeed(100, 800);
+  $(function(){
+    $.scrollUp();
+  });
+  $(function() {
+      // Default
+      jQuery.scrollSpeed(100, 800);
 
-    // Custom Easing
-    jQuery.scrollSpeed(100, 800, 'easeOutCubic');
+      // Custom Easing
+      jQuery.scrollSpeed(100, 800, 'easeOutCubic');
+  });
 
-});
+
+console.log("Hello World!");
